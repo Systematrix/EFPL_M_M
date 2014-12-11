9a2d3fead3670ac2f0f6643c24b94ebe5dfe3769
Sambhaji <kolate.sambhaji@gmail.com>
2014-12-11 10:37:16 +0000

    added manufacturing report

---
+++ efpl_manufacturing/efpl_manufacturing/doctype/ripe_fruit_issue_slip/test_ripe_fruit_issue_slip.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, Systematrix and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Ripe Fruit Issue Slip')
+
+class TestRipeFruitIssueSlip(unittest.TestCase):
+	pass
