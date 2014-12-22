5d5f823a198b11f84980f6e9ead5ba5ec70e3c2c
Sambhaji <kolate.sambhaji@gmail.com>
2014-12-22 07:54:20 +0000

    form added for daily sanitation

---
+++ efpl_manufacturing/maintenance/doctype/daily_sanitation_check_list/test_daily_sanitation_check_list.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2013, Systematrix and Contributors
+# See license.txt
+
+import frappe
+import unittest
+
+test_records = frappe.get_test_records('Daily Sanitation Check List')
+
+class TestDailySanitationCheckList(unittest.TestCase):
+	pass
